ea500082f4bb5311981b75e157ccfca43611f639
Marko Steinberger <marko.steinberger@gmail.com>
2023-10-07 02:46:51 +0200

    Navbar und DataTable eingefügt.

---
+++ poc/src/lib/root/MainNavBar.svelte
@@ -0,0 +1,34 @@
+<script>
+    import {
+        Navbar,
+        NavBrand,
+        NavLi,
+        NavUl,
+        NavHamburger,
+        ImagePlaceholder,
+        Skeleton,
+        TextPlaceholder,
+    } from "flowbite-svelte";
+</script>
+
+<Navbar>
+    <NavBrand href="/">
+        <img
+            src="/images/flowbite-svelte-icon-logo.svg"
+            class="mr-3 h-6 sm:h-9"
+            alt="Flowbite Logo"
+        />
+        <span
+            class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+            >IPS-C</span
+        >
+    </NavBrand>
+    <NavHamburger />
+    <NavUl>
+        <NavLi href="/">Home</NavLi>
+        <NavLi href="/about">IPS-C.Next</NavLi>
+        <NavLi href="/docs/components/navbar">IPS-C Legacy</NavLi>
+        <NavLi href="/pricing">Help</NavLi>
+        <NavLi href="/contact">Login</NavLi>
+    </NavUl>
+</Navbar>
